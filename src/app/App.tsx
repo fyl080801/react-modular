3ebf787f32f46472a8ea45f077ae5cd46fd88f09
fyl080801 <fyl080801@homtail.com>
2019-02-20 01:33:49 +0800

    feat:

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -10,25 +10,19 @@ import '../modules/m1/module'
 // 在没有集成路由加载的时候先固定引用组件
 import TestView from '../modules/m1/views/TestView'
 
-class App extends React.Component {
-  public render() {
-    return (
-      // 使用一个 Provider 作为最外层容器传递一个 store(如果用react-redux提供的provider也行)
-      // store 的初始化时会根据模块自动初始化 reducer 逻辑
-      <ModularProvider store={createModularStore()}>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React...</h1>
-          </header>
-          <p className="App-intro">
-            To get started, edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <TestView />
-        </div>
-      </ModularProvider>
-    )
-  }
+export default () => {
+  return (
+    <ModularProvider store={createModularStore()}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React...</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <TestView />
+      </div>
+    </ModularProvider>
+  )
 }
-
-export default App
